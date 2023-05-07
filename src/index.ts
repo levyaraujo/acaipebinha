b829eb442a774e0ee644dbb7a6934df4e438f1cc
Levy Araujo <lev0x79@gmail.com>
2023-05-07 18:53:32 -0300

    Allow all ports from localhost

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-import express from 'express';
-import { router } from './router';
-import mongoose from 'mongoose';
-import * as dotenv from 'dotenv';
-import http from 'node:http';
-import { Server } from 'socket.io';
+import express from "express";
+import { router } from "./router";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+import http from "node:http";
+import { Server } from "socket.io";
 
 dotenv.config();
-
-
 
 const uri = process.env.MONGODB_URI;
 const app = express();
@@ -15,10 +13,11 @@ const server = http.createServer(app);
 export const io = new Server(server);
 const staticPath = String(process.env.IMAGES_PATH);
 
-mongoose.set('strictQuery', false);
-mongoose.connect(String(uri))
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(String(uri))
   .then(() => {
-    app.use('/images', express.static(staticPath));
+    app.use("/images", express.static(staticPath));
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
     app.use(router);
@@ -27,9 +26,8 @@ mongoose.connect(String(uri))
     server.listen(port, () => {
       console.log(`Server is running on http://localhost:${port} 🚀`);
     });
-
   })
 
-  .catch(error => {
+  .catch((error) => {
     console.log(`Error on connecting to database: ${error}`);
   });
