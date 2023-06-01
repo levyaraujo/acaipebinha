0ce596a01f043c50226758ecc99618967d3e7040
Levy Araujo <lev0x79@gmail.com>
2023-06-01 18:04:44 -0300

    Integrate with OPENAI

--- src/bot/messageHandler.ts
+++ src/bot/messageHandler.ts
@@ -1,46 +1,50 @@
-import axios from 'axios';
-import dotenv from 'dotenv';
+import axios from "axios";
+import dotenv from "dotenv";
+import { aiResponse } from "../lib/openAI.ts/openai";
 
 dotenv.config();
 
 export default class Bot {
-	profileName: string;
-	phoneNumber: string;
+  profileName: string;
+  phoneNumber: string;
 
-	constructor(profileName: string, phoneNumber: string) {
-		this.profileName = profileName;
-		this.phoneNumber = phoneNumber;
-	}
+  constructor(profileName: string, phoneNumber: string) {
+    this.profileName = profileName;
+    this.phoneNumber = phoneNumber;
+  }
 
-	private async sendMessage(recipient: string, message: string) {
-		const url = `${process.env.WHATSAPP_URL}`;
-		const config = {
-			headers: {
-				'Authorization': `Bearer ${process.env.TOKEN}`,
-				"Content-Type": "application/json"
-			}
-		};
+  private async sendMessage(recipient: string, message: string) {
+    const url = `${process.env.WHATSAPP_URL}`;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${process.env.TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    };
 
-		const payload = {
-			"messaging_product": "whatsapp",
-			"preview_url": false,
-			"recipient_type": "individual",
-			"to": recipient,
-			"type": "text",
-			"text": {
-				"body": message
-			}
-		};
+    const payload = {
+      messaging_product: "whatsapp",
+      preview_url: false,
+      recipient_type: "individual",
+      to: recipient,
+      type: "text",
+      text: {
+        body: message,
+      },
+    };
 
-		try {
-			const response = await axios.post(url, payload, config);
-			return response;
-		} catch (error) {
-			console.log(error);
-		}
-	}
+    try {
+      const response = await axios.post(url, payload, config);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	onboard() {
-		this.sendMessage(this.phoneNumber, `Olá, *${this.profileName}*! Tudo bem? Acesse o link abaixo e faça seu pedido! 🥰 \n\nhttps://acaipebinha.tunnelto.dev/`);
-	}
+  onboard() {
+    this.sendMessage(
+      this.phoneNumber,
+      `Olá, *${this.profileName}*! Tudo bem? Acesse o link abaixo e faça seu pedido! 🥰 \n\nhttps://acaipebinha.tunnelto.dev/`
+    );
+  }
 }
--- src/bot/messageListener.ts
+++ src/bot/messageListener.ts
@@ -1,25 +1,24 @@
-import { Request, Response } from 'express';
-import Bot from './messageHandler';
-
+import { Request, Response } from "express";
+import Bot from "./messageHandler";
 
 export async function userMessageHandler(request: Request, response: Response) {
-	const data = request.body;
-	console.log(data);
-	let profileName: string;
-	const message = data.entry[0].changes[0].value;
+  const data = request.body;
+  console.log(data);
+  let profileName: string;
+  const message = data.entry[0].changes[0].value;
+  console.log(JSON.stringify(data.entry[0].changes[0]));
 
-	try {
+  try {
+    if ("contacts" in data.entry[0].changes[0].value && "messages" in message) {
+      profileName = message.contacts[0].profile.name;
+      const phoneNumber = message.contacts[0].wa_id;
+      const bot = new Bot(profileName, phoneNumber);
+      bot.onboard();
+    }
 
-		if ('contacts' in data.entry[0].changes[0].value && 'messages' in message) {
-			profileName = message.contacts[0].profile.name;
-			const phoneNumber = message.contacts[0].wa_id;
-			const bot = new Bot(profileName, phoneNumber);
-			bot.onboard();
-		}
-
-		return response.sendStatus(200);
-	} catch (error) {
-		console.error({ error });
-		return response.sendStatus(500);
-	}
+    return response.sendStatus(200);
+  } catch (error) {
+    console.error({ error });
+    return response.sendStatus(500);
+  }
 }
