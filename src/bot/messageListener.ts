648b5b54d2cc09c700f35be3165e054850ca1ed2
Levy Araujo <lev0x79@gmail.com>
2023-06-01 18:41:25 -0300

    Fix JSON parse error

--- src/bot/messageListener.ts
+++ src/bot/messageListener.ts
@@ -1,12 +1,33 @@
 import { Request, Response } from "express";
 import Bot from "./messageHandler";
 
+const content = {
+  value: {
+    messaging_product: "whatsapp",
+    metadata: {
+      display_phone_number: "15550910644",
+      phone_number_id: "103179066135470",
+    },
+    contacts: [{ profile: { name: "Levy" }, wa_id: "559481362600" }],
+    messages: [
+      {
+        from: "559481362600",
+        id: "wamid.HBgMNTU5NDgxMzYyNjAwFQIAEhgSQzUwQkE5RUUwQTg4QjI4Q0QyAA==",
+        timestamp: "1685653761",
+        text: { body: "olá" },
+        type: "text",
+      },
+    ],
+  },
+  field: "messages",
+};
+
 export async function userMessageHandler(request: Request, response: Response) {
   const data = request.body;
   console.log(data);
   let profileName: string;
   const message = data.entry[0].changes[0].value;
-  const userMessage = message.messages[0] ? message.messages[0].text.body : "";
+  const userMessage = message.messages[0].text.body;
   console.log(JSON.stringify(data.entry[0].changes[0]));
 
   try {
